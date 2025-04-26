d45ed8192ad93ea87305e975c7e161375fd5abb6
Romeo Mureithi <romeomureithi@gmail.com>
2025-04-26 15:04:43 +0300

    product controller

---
+++ ecommerce-backend/controllers/product.go
@@ -0,0 +1,12 @@
+package controllers
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func GetProducts(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "List of products",
+	})
+}
