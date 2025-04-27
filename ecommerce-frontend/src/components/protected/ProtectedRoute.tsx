294c6758362e58aff2f37245d68caffc670cb383
Romeo Mureithi <romeomureithi@gmail.com>
2025-04-27 12:50:38 +0300

    protected route

---
+++ ecommerce-frontend/src/components/protected/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { isAuthenticated } from "@/lib/auth";
+
+export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!isAuthenticated()) {
+            router.push("/login");
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return <>{children}</>
+}
