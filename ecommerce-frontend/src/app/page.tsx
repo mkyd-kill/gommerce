ebb40604d7dc1dc9c92ea207a6260d019d8b892c
Romeo Mureithi <romeomureithi@gmail.com>
2025-04-27 12:49:29 +0300

    auth pages update

--- ecommerce-frontend/src/app/page.tsx
+++ ecommerce-frontend/src/app/page.tsx
@@ -1,2 +1,7 @@
 export default function Home() {
+    return (
+        <div className="max-w-md">
+            <h2 className="text-2xl-font-bold">Shopping at your fingertips!</h2>
+        </div>
+    )
 }
--- ecommerce-frontend/src/app/register/page.tsx
+++ ecommerce-frontend/src/app/register/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { register } from "@/services/auth";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function RegisterPage() {
   const router = useRouter();
