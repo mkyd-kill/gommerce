337468a83645951b42b75dcc63478be2c02d0e36
Romeo Mureithi <romeomureithi@gmail.com>
2025-04-27 11:42:15 +0300

    updated product model

--- ecommerce-backend/models/Product.go
+++ ecommerce-backend/models/Product.go
@@ -1,1 +1,15 @@
 package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Name string `json:"name"`
+	Description string `json:"description"`
+	Price float64 `json:"price"`
+	Category string `json:"category"`
+	ImageURL string `json:"image_url"`
+	Stock int `json:"stock"`
+}
