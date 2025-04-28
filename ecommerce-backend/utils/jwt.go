36b737308ce185bddd88c0d3b9c7766addfaf254
Romeo Mureithi <romeomureithi@gmail.com>
2025-04-28 17:28:53 +0300

    generating token

---
+++ ecommerce-backend/utils/jwt.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"time"
+	"os"
+	"github.com/dgrijalva/jwt-go"
+)
+
+var jwtKey = []byte(os.Getenv("JWT_SECRET"))
+
+type Claims struct {
+	Username string		`json:"username"`
+	Email string		`json:"email"`
+	jwt.StandardClaims
+}
+
+func GenerateToken(username, email string, duration time.Duration) (string, error) {
+	claims := &Claims{
+		Username: username,
+		Email: email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(duration).Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtKey)
+}
