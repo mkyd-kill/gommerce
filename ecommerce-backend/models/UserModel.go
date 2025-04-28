a919e56fa46472cb275327dc70f0c78745f3263b
Romeo Mureithi <romeomureithi@gmail.com>
2025-04-28 17:26:41 +0300

    added user model

---
+++ ecommerce-backend/models/UserModel.go
@@ -0,0 +1,12 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username string		`json:"username" gorm:"uniqueIndex"`
+	Email string		`json:"email" gorm:"uniqueIndex"`
+	Password string		`json:"-"`
+}
