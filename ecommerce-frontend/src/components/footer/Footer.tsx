4f60864cba5124263e0f6179fd5f6d15a91ca1b7
Romeo Mureithi <romeomureithi@gmail.com>
2025-04-28 13:48:13 +0300

    footer init

---
+++ ecommerce-frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <div className="mx-4 xs:mx-2 sm:mx-2 flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 gap-8 px-4 pb-12 text-white">
+            <h2>Footer Component: (Comming Soon)</h2>
+        </div>
+    )
+}
