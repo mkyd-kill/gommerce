558d05474acda444048d8e6e873039762748ddb1
Romeo Mureithi <romeomureithi@gmail.com>
2025-04-29 17:17:59 +0300

    product category ui

--- ecommerce-frontend/src/app/page.tsx
+++ ecommerce-frontend/src/app/page.tsx
@@ -1,7 +1,9 @@
+import CategoryCard from "@/components/category/CategoryCard";
+
 export default function Home() {
     return (
-        <div className="max-w-md">
-            <h2 className="text-2xl-font-bold">Shopping at your fingertips!</h2>
+        <div className="container">
+            <CategoryCard />
         </div>
     )
 }
