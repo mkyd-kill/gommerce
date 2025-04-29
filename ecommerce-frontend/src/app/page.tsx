2c82e728f75460d7be8d2bd22bef70906ae9ba69
Romeo Mureithi <romeomureithi@gmail.com>
2025-04-29 22:58:11 +0300

    footer ui

--- ecommerce-frontend/src/app/page.tsx
+++ ecommerce-frontend/src/app/page.tsx
@@ -3,18 +3,15 @@ import CategoryCard from "@/components/homepage/CategoryCard";
 import HotDeals from "@/components/homepage/HotDeals";
 import FeaturedProduct from "@/components/homepage/FeaturedProduct";
 import Trending from "@/components/homepage/Trending";
+import Brand from "@/components/homepage/Brand";
 
 export default function Home() {
   return (
-    <div
-      className="container"
-      style={{
-        maxWidth: "1280px",
-      }}
-    >
+    <div className="container">
       <CategoryCard />
       <HotDeals />
       <FeaturedProduct />
+      <Brand />
       <Trending />
     </div>
   );
