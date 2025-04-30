c1f24ad55304cb739ce31c574fa784ef99c23563
Romeo Mureithi <romeomureithi@gmail.com>
2025-04-30 18:11:13 +0300

    protected cart view

--- ecommerce-frontend/src/app/cart/page.tsx
+++ ecommerce-frontend/src/app/cart/page.tsx
@@ -1,12 +1,10 @@
-import ProtectedRoute from "@/components/protected/ProtectedRoute";
+import ProtectedRoute from "@/lib/ProtectedRoute";
 
 export default function CartPage() {
     return (
         <ProtectedRoute>
-            <>
             <h1 className="text-xl font-bold mb-4">Your Cart</h1>
             <p>(Coming Soon: cart functionality)</p>
-            </>
         </ProtectedRoute>
     );
 }
