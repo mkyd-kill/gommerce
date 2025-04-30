7f6a58c54608f6cc1b161a5e6b624bcdc409b5fc
Romeo Mureithi <romeomureithi@gmail.com>
2025-04-30 18:12:03 +0300

    context wrapper, toastify wrapper

--- ecommerce-frontend/src/app/layout.tsx
+++ ecommerce-frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 import { ToastContainer } from "react-toastify";
+import { UserProvider } from "@/context/useAuth";
 import "./globals.css";
 
 export default function RootLayout({
@@ -12,12 +13,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Navbar />
-        <main className="m-2 p-2">
-          <ToastContainer />
-          {children}
+        <UserProvider>
+          <Navbar />
+          <main className="m-2 p-2">
+            <ToastContainer />
+            {children}
           </main>
-        <Footer />
+          <Footer />
+        </UserProvider>
       </body>
     </html>
   );
--- ecommerce-frontend/src/lib/auth.ts
+++
@@ -1,4 +0,0 @@
-export const isAuthenticated = () => {
-    if (typeof window ===  "undefined") return false;
-    return !!localStorage.getItem("token");
-}
