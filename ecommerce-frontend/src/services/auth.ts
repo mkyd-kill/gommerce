11268992990eb0df9cf0584ff553cc5515e0f6f7
Romeo Mureithi <romeomureithi@gmail.com>
2025-04-30 18:08:34 +0300

    updated auth api

--- ecommerce-frontend/src/services/auth.ts
+++ ecommerce-frontend/src/services/auth.ts
@@ -1,12 +1,32 @@
 import api from "@/lib/axios";
 import { AuthResponse } from "@/types/user";
+import { handleError } from "@/helpers/ErrorHandler";
 
-export const login = async (email: string, password: string): Promise<AuthResponse> => {
-    const res = await api.post<AuthResponse>("/users/login", { email, password });
-    return res.data;
+export const loginAPI = async (email: string, password: string) => {
+  try {
+    const res = await api.post<AuthResponse>("/users/login", {
+      email,
+      password,
+    });
+    return res;
+  } catch (error) {
+    handleError(error);
+  }
 };
 
-export const register = async (username: string, email: string, password: string) => {
-    const res = await api.post("/users/register", { username, email, password });
-    return res.data;
-}
+export const registerAPI = async (
+  username: string,
+  email: string,
+  password: string
+) => {
+  try {
+    const res = await api.post("/users/register", {
+      username,
+      email,
+      password,
+    });
+    return res;
+  } catch (error) {
+    handleError(error);
+  }
+};
