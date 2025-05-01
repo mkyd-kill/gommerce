2d9c557418a75ef9d5564a0c84a03a4ff4622cd6
Romeo Mureithi <romeomureithi@gmail.com>
2025-05-01 14:45:55 +0300

    enhanced procted routes

--- ecommerce-frontend/src/lib/ProtectedRoute.tsx
+++ ecommerce-frontend/src/lib/ProtectedRoute.tsx
@@ -1,14 +1,23 @@
 "use client";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/context/useAuth";
+import { toast } from "react-toastify";
 
 export default function ProtectedRoute ({ children }: { children: React.ReactNode }) {
+    const [checked, setChecked] = useState(false);
     const router = useRouter();
     const { isLoggedIn } = useAuth();
+    useEffect(() => {
+        if (!isLoggedIn()) {
+            router.push("/login");
+            toast.warning("Authentication required to access page");
 
-    return isLoggedIn() ? (
-        <>{children}</>
-    ) : (
-        router.push("/login")
-    );
+        } else {
+            setChecked(true);
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return checked ? <>{children}</> : null;
 };
