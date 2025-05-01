c51aab62b5664e534615833aeef73649692bd413
Romeo Mureithi <romeomureithi@gmail.com>
2025-05-01 15:44:00 +0300

    cart context wrapper

--- ecommerce-frontend/src/app/layout.tsx
+++ ecommerce-frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@
 import Navbar from "@/components/navbar/Navbar";
 import { ToastContainer } from "react-toastify";
 import { UserProvider } from "@/context/useAuth";
+import { CartProvider } from "@/context/CartContext";
 import "./globals.css";
 
 export default function RootLayout({
@@ -15,10 +16,12 @@ export default function RootLayout({
       <body>
         <UserProvider>
           <Navbar />
+          <CartProvider>
           <main className="m-2 p-2">
             <ToastContainer />
             {children}
           </main>
+          </CartProvider>
         </UserProvider>
       </body>
     </html>
