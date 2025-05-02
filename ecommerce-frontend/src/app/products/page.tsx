56f89a3c59ec13aa12850a20fef5cc4115e0b9d7
Romeo Mureithi <romeomureithi@gmail.com>
2025-05-02 12:19:55 +0300

    price formating

--- ecommerce-frontend/src/app/products/page.tsx
+++ ecommerce-frontend/src/app/products/page.tsx
@@ -17,10 +17,10 @@ export default function ProductPage() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
       {products.map((prod) => (
-        <div className="border p-4 rounded-shadow" key={prod.id}>
+        <div className="border p-4 rounded-xl" key={prod.id}>
           <h2 className="text-lg font-bold">{prod.name}</h2>
           <p>{prod.description}</p>
-          <p className="text-blue-600 font-semibold">${prod.price}</p>
+          <p className="text-blue-600 font-semibold">Kshs. {prod.price.toLocaleString()}</p>
           <button className="bg-green-600 text-white px-4 py-1 mt-2 rounded">
             Add to Cart
           </button>
