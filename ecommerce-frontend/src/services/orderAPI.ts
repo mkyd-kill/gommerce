750e650d109b89c1933d75fe51c4c7a135004cad
Romeo Mureithi <romeomureithi@gmail.com>
2025-05-02 22:40:51 +0300

    get orders api

--- ecommerce-frontend/src/services/orderAPI.ts
+++ ecommerce-frontend/src/services/orderAPI.ts
@@ -1,8 +1,13 @@
 import api from "@/lib/axios";
 
-const baseUrl = "/user/order/"
+const baseUrl = "/order/"
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const createOrderAPI = async (order: any) => {
     return await api.post(baseUrl + "create", order);
 };
+
+export const getOrdersAPI = async (email: string) => {
+    const res = await api.get(baseUrl + `?email=${email}`);
+    return res.data;
+}
