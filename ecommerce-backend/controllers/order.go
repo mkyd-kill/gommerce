94c21e8aa58693ac42bb3f93777a47509ac413cc
Romeo Mureithi <romeomureithi@gmail.com>
2025-05-02 23:31:09 +0300

    get order by user

--- ecommerce-backend/controllers/order.go
+++ ecommerce-backend/controllers/order.go
@@ -22,3 +22,20 @@ func CreateOrder(c *gin.Context) {
 
 	c.JSON(http.StatusCreated, gin.H{"message": "Order Created Successfully"})
 }
+
+func GetOrderByUser(c *gin.Context) {
+	userEmail := c.Query("email")
+
+	if userEmail == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"Bad Request": "Email is required"})
+		return 
+	}
+
+	var orders []models.Order
+	if err := database.DB.Preload("Items").Where("user_email = ?", userEmail).Find(&orders).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Server Error": "Could not fetch orders"})
+		return
+	}
+
+	c.JSON(http.StatusOK, orders)
+}
