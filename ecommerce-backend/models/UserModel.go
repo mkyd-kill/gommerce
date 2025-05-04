7a7ef62760244fd896c4399f070e6a5dfd019b9d
Romeo Mureithi <romeomureithi@gmail.com>
2025-05-04 15:31:41 +0300

    updated model

--- ecommerce-backend/models/UserModel.go
+++ ecommerce-backend/models/UserModel.go
@@ -6,6 +6,8 @@ import (
 
 type User struct {
 	gorm.Model
+	Firstname string	`json:"first_name"`
+	Lastname string		`json:"last_name"`
 	Username string		`json:"username" gorm:"uniqueIndex"`
 	Email string		`json:"email" gorm:"uniqueIndex"`
 	Password string		`json:"-"`
