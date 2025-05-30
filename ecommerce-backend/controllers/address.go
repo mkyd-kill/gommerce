9eca2a58c34bd230d4643edfc3383a00bdba7567
Romeo Mureithi <romeomureithi@gmail.com>
2025-05-30 15:20:09 +0300

    address controller

---
+++ ecommerce-backend/controllers/address.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"ecommerce-backend/models"
+	"github.com/gin-gonic/gin"
+	"ecommerce-backend/database"
+)
+
+func CreateAddress(c *gin.Context) {
+
+}
+
+func UpdateAddress(c *gin.Context) {
+
+}
+
+func DeleteAddress(c *gin.Context) {
+	
+}
