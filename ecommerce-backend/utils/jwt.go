0536d61825c4f127e7d3d7685e306fd1c63bfed8
Romeo Mureithi <romeomureithi@gmail.com>
2025-06-03 19:51:53 +0300

    fix token generate

--- ecommerce-backend/utils/jwt.go
+++ ecommerce-backend/utils/jwt.go
@@ -1,28 +1,28 @@
 package utils
 
 import (
-	"time"
-	"os"
-	jwt "github.com/dgrijalva/jwt-go"
+    "time"
+    "os"
+    jwt "github.com/dgrijalva/jwt-go"
 )
 
 var jwtKey = []byte(os.Getenv("JWT_SECRET"))
 
 type Claims struct {
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	jwt.StandardClaims
+    Username string `json:"username"`
+    Email    string `json:"email"`
+    jwt.StandardClaims
 }
 
 func GenerateToken(username string, email string, duration time.Duration) (string, error) {
-	claims := &Claims{
-		Username: username,
-		Email:    email,
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: time.Now().Local().Add(duration).Unix(),
-		},
-	}
-	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString(jwtKey)
-
-	return token, err
+    claims := &Claims{
+        Username: username,
+        Email:    email,
+        StandardClaims: jwt.StandardClaims{
+            Subject:   email,
+            ExpiresAt: time.Now().Add(duration).Unix(),
+        },
+    }
+    token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString(jwtKey)
+    return token, err
 }
