052110d2a8ea676efe4e85738a456efcb8d91259
Romeo Mureithi <romeomureithi@gmail.com>
2025-06-06 18:20:18 +0300

    homepage update

--- ecommerce-frontend/src/app/(marketing)/page.tsx
+++ ecommerce-frontend/src/app/(marketing)/page.tsx
@@ -1,19 +1,23 @@
-"use client";
 import Link from "next/link";
+import Image from "next/image";
 import Footer from "@/components/footer/Footer";
+import { fetchAllProducts } from "@/services/productAPI";
+import banner from "../../assets/defaults/banner.svg";
 
-export default function Home() {
+export default async function Home() {
+  const products = await fetchAllProducts();
+
   return (
     <div className="container">
-      <section>
-        <div>
-          <div>
-            <h2>Welcome to Gommerce Ecommerce</h2>
-            <p>Discover the latest products at the best prices</p>
+      <section className="rounded bg-gray-100 py-6 sm:py-12">
+        <div className="mx-auto grid grid-cols-2 items-center justify-items-center gap-8 px-8 sm:px-16">
+          <div className="max-w-md space-y-4">
+            <h2 className="text-3xl font-bold tracking-tight md:text-4xl">Welcome to Gommerce Ecommerce</h2>
+            <p className="text-gray-600">Discover the latest products at the best prices</p>
             <button className="myBtn flex-grow-0 flex-shrink-0 text-sm font-medium text-center text-white">
               <Link
                 href="/products"
-                className="myBtn flex justify-center items-center self-stretch flex-grow-0 flex-shrink-0 relative overflow-hidden gap-2 px-3.5 py-2 rounded-lg bg-[#66004b] border border-[#66004b]"
+                className="myBtn flex justify-center items-center self-stretch flex-grow-0 flex-shrink-0 relative overflow-hidden gap-2 px-3.5 py-2 rounded-lg bg-[#66004b] border border-[#66004b] rounded-full"
               >
                 <p className="flex-grow-0 flex-shrink-0 text-sm font-medium text-center text-white">
                   Browse All Products
@@ -21,8 +25,10 @@ export default function Home() {
               </Link>
             </button>
           </div>
+          <Image className="rounded-xl" alt="Banner Image" src={banner} width={500} height={400}/>
         </div>
       </section>
+      <section className="py-6"></section>
       <Footer />
     </div>
   );
