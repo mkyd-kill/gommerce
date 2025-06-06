7cc3d22a249f1b213af3fa399c2bc2367ba203fd
Romeo Mureithi <romeomureithi@gmail.com>
2025-06-06 17:15:43 +0300

    update

--- ecommerce-frontend/src/app/(marketing)/page.tsx
+++ ecommerce-frontend/src/app/(marketing)/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import CategoryCard from "@/components/homepage/CategoryCard";
 import HotDeals from "@/components/homepage/HotDeals";
 import FeaturedProduct from "@/components/homepage/FeaturedProduct";
 import Trending from "@/components/homepage/Trending";
@@ -9,7 +8,6 @@ import Footer from "@/components/footer/Footer";
 export default function Home() {
   return (
     <div className="container">
-      <CategoryCard />
       <HotDeals />
       <FeaturedProduct />
       <Brand />
