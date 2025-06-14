e5fe6e3614f8826f9c7216906e6f08cb3b9a5aa4
Romeo Mureithi <romeomureithi@gmail.com>
2025-06-15 02:29:19 +0300

    updated footer

--- ecommerce-frontend/src/components/footer/Footer.tsx
+++ ecommerce-frontend/src/components/footer/Footer.tsx
@@ -1,9 +1,9 @@
 function Footer() {
   return (
     <div
-      className="flex flex-col justify-center items-center self-stretch flex-grow-0 flex-shrink-0 px-2 pb-3 bg-white rounded-xl"
+      className="flex flex-col justify-center items-center self-stretch flex-grow-0 flex-shrink-0 px-1 pb-2 bg-white rounded-xl"
     >
-      <span className="flex flex-grow text-gray-500 text-sm text-center pt-4">
+      <span className="flex flex-grow text-[#475467] font-semibold text-xs text-center pt-2.5">
         Developed by Romeo Namunde Mureithi
       </span>
     </div>
