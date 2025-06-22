1729e0b2f81e83d47239b386a84b5d94c283e0ae
Romeo Mureithi <romeomureithi@gmail.com>
2025-06-22 23:38:40 +0300

    middleware cookie

--- ecommerce-backend/middleware/authmiddleware.go
+++ ecommerce-backend/middleware/authmiddleware.go
@@ -11,26 +11,24 @@ import (
 
 func GetAuthMiddlewareFunc(tokenMaker *token.JWTMaker) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		authHeader := c.GetHeader("Authorization")
+		// getting cookie of req
+		authHeader, err := c.Cookie("auth-token")
+		if err != nil {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+
 		if authHeader == "" {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "authorization header missing"})
 			return
 		}
 
-		fields := strings.Fields(authHeader)
-		if len(fields) != 2 || strings.ToLower(fields[0]) != "Bearer" {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid authorization format"})
-			return
-		}
-
-		tokenStr := fields[1]
-		claims, err := tokenMaker.VerifyToken(tokenStr)
+		claims, err := tokenMaker.VerifyToken(authHeader)
 		if err != nil {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": fmt.Sprintf("token error: %v", err)})
 			return
 		}
 
-		c.Set("authUser", claims)
+		c.Set("auth-user", claims)
 		c.Next()
 	}
 }
