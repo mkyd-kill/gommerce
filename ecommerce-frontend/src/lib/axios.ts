6de62f94b531e67c6a7f73de431927bfd5a67884
Romeo Mureithi <romeomureithi@gmail.com>
2025-06-27 02:29:18 +0300

    fix axios api

--- ecommerce-frontend/src/lib/axios.ts
+++ ecommerce-frontend/src/lib/axios.ts
@@ -3,9 +3,6 @@ import axios from "axios";
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_BASE_URL!,
     withCredentials: true,
-    headers: {
-        "Access-Control-Allow-Origin": "*",
-      }
 });
 
 export default api;
