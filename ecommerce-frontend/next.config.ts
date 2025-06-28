17d0fa9ac13ded0729be90c25526f76562363086
Romeo Mureithi <romeomureithi@gmail.com>
2025-06-28 17:38:01 +0300

    image remote patterns

--- ecommerce-frontend/next.config.ts
+++ ecommerce-frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: []
+    remotePatterns: [new URL("http://localhost.images/*")]
   }
 };
 
