2bc039c4fa2b2b48cdfee35c5c3c47cf3e9d0510
Romeo Mureithi <romeomureithi@gmail.com>
2025-06-28 21:22:00 +0300

    updated ui

--- ecommerce-frontend/src/app/(admin)/admin/products/new/page.tsx
+++ ecommerce-frontend/src/app/(admin)/admin/products/new/page.tsx
@@ -4,15 +4,21 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
+import { CreateProduct } from "@/services/productAPI";
 
 export default function NewProductPage() {
   const router = useRouter();
   const [form, setForm] = useState({
+    id: 0,
     name: "",
     description: "",
-    price: "",
-    stock: "",
+    price: 0,
     category: "",
+    image: "",
+    featured: false, // boolean value
+    stock: 0,
+    quantity: 0,
+    rating: 0
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -22,12 +28,8 @@ export default function NewProductPage() {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const res = await fetch("/api/products", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(form),
-      });
-      if (!res.ok) throw new Error();
+      const res = await CreateProduct(form)
+      if (!res) throw new Error();
       toast.success("Product created!");
       router.push("/admin/products");
     } catch {
@@ -40,11 +42,15 @@ export default function NewProductPage() {
       <h1 className="text-xl font-bold mb-4">Add New Product</h1>
       <form onSubmit={handleSubmit} className="space-y-4 max-w-xl">
         <Input name="name" label="Name" value={form.name} onChange={handleChange} />
-        <Input name="price" label="Price" type="number" value={form.price} onChange={handleChange} />
-        <Input name="stock" label="Stock" type="number" value={form.stock} onChange={handleChange} />
-        <Input name="category" label="Category" value={form.category} onChange={handleChange} />
         <TextArea name="description" label="Description" value={form.description} onChange={handleChange} />
-        <button type="submit" className="bg-[#66004b] text-white px-4 py-2 rounded cursor-pointer">
+        <Input name="price" label="Price" type="number" value={form.price} onChange={handleChange} />
+        <Input name="category" label="Category" value={form.category} onChange={handleChange} />
+        <Input name="image" label="Image" type="file" value={form.image} onChange={handleChange} />
+        <Input name="featured" label="Featured" value={form.featured} onChange={handleChange} />
+        <Input name="stock" label="Stock" type="number" value={form.stock} onChange={handleChange} />
+        <Input name="quantity" label="Quantity" type="number" value={form.quantity} onChange={handleChange} />
+        <Input name="rating" label="Rating" type="number" value={form.rating} onChange={handleChange} />
+        <button type="submit" className="bg-[#66004b] text-white px-4 py-2 rounded cursor-pointer justify-right text-right">
           Create Product
         </button>
       </form>
