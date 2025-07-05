ff16c6498a145bf44868ca5fdbe7304c70195c82
Romeo Mureithi <romeomureithi@gmail.com>
2025-07-05 19:38:34 +0300

    user context wrapper

--- ecommerce-frontend/src/app/(admin)/layout.tsx
+++ ecommerce-frontend/src/app/(admin)/layout.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { UserProvider } from "@/context/useAuth";
 import Sidebar from "@/components/sidebar/Sidebar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -12,7 +13,7 @@ import {
   Receipt,
   LifeBuoy,
   LogOut,
-  Link
+  Link,
 } from "lucide-react";
 import "../globals.css";
 
@@ -25,29 +26,39 @@ export default function AdminLayout({
     <html lang="en">
       <title>Gommerce | Admin</title>
       <body>
-        <div className="flex min-h-screen">
-          <Sidebar>
-            <SidebarItem
-              icon={<LayoutDashboard size={20} />}
-              text="Dashboard"
-              active
-              href="/admin"
-            />
-            <SidebarItem icon={<UserCircle size={20} />} text="Users" />
-            <SidebarItem icon={<Boxes size={20} />} text="Products" href="/admin/products" />
-            <SidebarItem icon={<Receipt size={20} />} text="Billings" />
-            <SidebarItem icon={<Package size={20} />} text="Orders" />
-            <hr className="my-3" />
-            <SidebarItem icon={<Link size={20} />} text="Main Site" href="/" />
-            <SidebarItem icon={<Settings size={20} />} text="Settings" />
-            <SidebarItem icon={<LifeBuoy size={20} />} text="Help" />
-            <SidebarItem icon={<LogOut size={20} />} text="Logout" />
-          </Sidebar>
-          <main className="flex-1 p-3">
-            <ToastContainer autoClose={2000} />
-            {children}
-          </main>
-        </div>
+        <UserProvider>
+          <div className="flex min-h-screen">
+            <Sidebar>
+              <SidebarItem
+                icon={<LayoutDashboard size={20} />}
+                text="Dashboard"
+                active
+                href="/admin"
+              />
+              <SidebarItem icon={<UserCircle size={20} />} text="Users" />
+              <SidebarItem
+                icon={<Boxes size={20} />}
+                text="Products"
+                href="/admin/products"
+              />
+              <SidebarItem icon={<Receipt size={20} />} text="Billings" />
+              <SidebarItem icon={<Package size={20} />} text="Orders" />
+              <hr className="my-3" />
+              <SidebarItem
+                icon={<Link size={20} />}
+                text="Main Site"
+                href="/"
+              />
+              <SidebarItem icon={<Settings size={20} />} text="Settings" />
+              <SidebarItem icon={<LifeBuoy size={20} />} text="Help" />
+              <SidebarItem icon={<LogOut size={20} />} text="Logout" />
+            </Sidebar>
+            <main className="flex-1 p-3">
+              <ToastContainer autoClose={2000} />
+              {children}
+            </main>
+          </div>
+        </UserProvider>
       </body>
     </html>
   );
