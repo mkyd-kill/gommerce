d4b12b297276b5f08c89d5cc01035e3c45d0229d
Romeo Mureithi <romeomureithi@gmail.com>
2025-07-06 17:30:09 +0300

    secure admin middleware

--- ecommerce-backend/middleware/authmiddleware.go
+++ ecommerce-backend/middleware/authmiddleware.go
@@ -4,7 +4,6 @@ import (
 	"ecommerce-backend/token"
 	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -36,22 +35,20 @@ func GetAuthMiddlewareFunc(tokenMaker *token.JWTMaker) gin.HandlerFunc {
 
 func GetAdminMiddlewareFunc(tokenMaker *token.JWTMaker) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		authHeader := c.GetHeader("Authorization")
+		authHeader, err := c.Cookie("auth-token")
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "authentication cookie missing or invalid"})
+			return
+		}
+
 		if authHeader == "" {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "authorization header missing"})
 			return
 		}
 
-		fields := strings.Fields(authHeader)
-		if len(fields) != 2 || strings.ToLower(fields[0]) != "Bearer" {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid authorization format"})
-			return
-		}
-
-		tokenStr := fields[1]
-		claims, err := tokenMaker.VerifyToken(tokenStr)
+		claims, err := tokenMaker.VerifyToken(authHeader)
 		if err != nil {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": fmt.Sprintf("token error: %v", err)})
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("token error: %v", err)})
 			return
 		}
 
