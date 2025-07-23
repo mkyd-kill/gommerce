a04b2332b52a5bc8392f1af52570063fa77dfcf3
Romeo Mureithi <romeomureithi@gmail.com>
2025-07-23 17:14:15 +0300

    fix model error

--- ecommerce-frontend/src/types/product.ts
+++ ecommerce-frontend/src/types/product.ts
@@ -1,38 +1,27 @@
 export interface ProductModel {
   ID: number;
-  name: string;
-  description: string;
-  price: number;
-  image: string;
-  featured: boolean;
-  stock: number;
-  quantity: number;
-  rating: number;
-};
-
-export interface CreateProductModel {
-  name: string;
-  description: string;
-  price: number;
-  image: string;
-  featured: boolean;
-  stock: number;
-  quantity: number;
-  rating: number;
+  Name: string;
+  Description: string;
+  Price: number;
+  Image: string;
+  Featured: boolean;
+  Stock: number;
+  Rating: number;
+  Quantity: number;
 };
 
 export interface CartItem {
   ID: number;
-  name: string;
-  price: number;
-  image: string | null;
-  quantity: number;
+  Name: string;
+  Price: number;
+  Image: string | null;
+  Quantity: number;
 }
 
 export interface WishlistItem {
   ID: number;
-  name: string;
-  price: number;
-  image: string | null;
-  quantity: number;
+  Name: string;
+  Price: number;
+  Image: string | null;
+  Quantity: number;
 }
