f0e7cc0a7557886df5e974de6fb7c50077c5b23f
Romeo Mureithi <romeomureithi@gmail.com>
2025-07-27 04:08:12 +0300

    edit page init

---
+++ ecommerce-frontend/src/app/(admin)/admin/products/edit/[id]/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { UpdateProductById } from "@/services/productAPI";
+import { Input, TextArea } from "@/components/resuables/form-inputs";
+
+export default function EditProductPage() {
+
+}
