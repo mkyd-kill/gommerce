5d0680a657bb1024a46fd1eca925de14774e0ff5
Romeo Mureithi <romeomureithi@gmail.com>
2025-08-14 19:12:27 +0300

    production ready image served

--- ecommerce-frontend/next.config.ts
+++ ecommerce-frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL("http://localhost:8080")]
+    remotePatterns: [new URL(`${process.env.NEXT_PUBLIC_STATIC_IMAGE_URL}`)]
   }
 };
 
